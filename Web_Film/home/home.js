b8bb4c1acc244a38ce22c8cd0feffbc4803e87b8
nhuhuy2k <66986033+nhuhuy2k@users.noreply.github.com>
2022-04-07 16:19:49 +0700

    Add files via upload

---
+++ Web_Film/home/home.js
@@ -0,0 +1,15 @@
+// let header= document.getElementsByClassName('js_header')
+
+// function changeColorHeader(){
+//     header.classlist.add('shink')
+// }
+
+let mordalTrailer = document.getElementById('modal_trailer');
+
+function openModalTrailer(){
+    mordalTrailer.classList.add("open_modal_trailer")
+}
+
+function closeModalTrailer(){
+    mordalTrailer.classList.remove("open_modal_trailer")
+}
