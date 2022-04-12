634bea6d4fe30494218afb0b2708bc7a6f56f301
NHUHUY <huynn2k000@gmail.com>
2022-04-12 16:02:32 +0700

    tao Modal

--- Web_Film/home/home.js
+++ Web_Film/home/home.js
@@ -1,24 +1,28 @@
 // // let header= document.getElementsByClassName('js_header')
 
-// // function changeColorHeader(){
-// //     header.classlist.add('shink')
-// // }
+// Modal 
+let mordalTrailer = document.getElementById('modal');
 
-// let mordalTrailer = document.getElementById('modal_trailer');
+function openModalTrailer() {
+    mordalTrailer.classList.add("open_modal")
+}
 
-// function openModalTrailer(){
-//     mordalTrailer.classList.add("open_modal_trailer")
-// }
+function closeModalTrailer() {
+    mordalTrailer.classList.remove("open_modal")
+}
 
-// function closeModalTrailer(){
-//     mordalTrailer.classList.remove("open_modal_trailer")
-// }
+let modalContainer = document.getElementsByClassName("modal_container");
+    
+
+// modalContainer.addEventListener('click', function (event) {
+//     event.stopPropagation()
+// })
 
 // header listenEvent
-window.onscroll = function(){
+window.onscroll = function () {
     let header = document.getElementById("header")
-   console.info(document.documentElement.scrollTop);
-    if(document.documentElement.scrollTop > 0 || document.body.scrollTop > 0){
+    console.info(document.documentElement.scrollTop);
+    if (document.documentElement.scrollTop > 0 || document.body.scrollTop > 0) {
         header.classList.add("shink")
     } else {
         header.classList.remove("shink")
@@ -32,10 +36,10 @@ function showSlider() {
     var i;
     var slides = document.getElementsByClassName("slide_item");
     for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+        slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlider, 10000);
-  }
+}
