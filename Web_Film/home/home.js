e592ee24fa4f58b0489c3976eed227908d654ee4
Daniel N <daniel@vmodev.com>
2022-04-21 22:15:32 +0700

    fix some issues

--- Web_Film/home/home.js
+++ Web_Film/home/home.js
@@ -4,27 +4,13 @@
 let modalTrailer = document.getElementById('modal');
 let iframeTrailer = document.getElementById("iframe_trailer");
 function openModalTrailer(url) {
-    iframeTrailer.setAttribute("src", url);
-    modalTrailer.style.display = "block";
+    console.log(1);
+    // iframeTrailer.setAttribute("src", url);
+    // modalTrailer.style.display = "block";
 }
 function closeModalTrailer() {
     modalTrailer.style.display = "none";
     iframeTrailer.setAttribute("src", "");
-}
-
-// Show Slider
-var slideIndex = 0;
-showSlider();
-function showSlider() {
-    var i;
-    var slides = document.getElementsByClassName("slide_item");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlider, 5000);
 }
 
 ////////////////////////////////////////////////////////////////////
