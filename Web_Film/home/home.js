0d8386ccedf9a730b8ae9957d599c80778e97246
NHUHUY <huynn2k000@gmail.com>
2022-05-18 08:22:15 +0700

    ae

--- Web_Film/home/home.js
+++ Web_Film/home/home.js
@@ -15,10 +15,10 @@ renderSlider()
 
 $(document).ready(function () {
     $(function () {
-        $("#header__page").load("../header/header.html");
+        $("#header__page").load("./header/header.html");
     });
     $(function () {
-        $("#footer__page").load("../footer/footer.html");
+        $("#footer__page").load("./footer/footer.html");
     });
 
 });
